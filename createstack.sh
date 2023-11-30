391cd8120df41449f8860e5d21562a53d084b8fc
UltrViolet <endgameviable@gmail.com>
2023-11-29 21:52:52 -0500

    fix metadata table partition key

--- createstack.sh
+++ createstack.sh
@@ -1,2 +1,3 @@
 eval "$(<.env)"
 aws cloudformation create-stack --stack-name $STACKNAME --template-body file://./endgameviable2024.yaml --capabilities CAPABILITY_IAM --profile default
+echo "don't forget that you'll need to set the Amplify service role to the new one created!"
--- deletestack.sh
+++ deletestack.sh
@@ -1,2 +1,3 @@
 eval "$(<.env)"
 aws cloudformation delete-stack --stack-name $STACKNAME --profile default
+echo "don't forget that the Amplify service role will also be deleted!"
