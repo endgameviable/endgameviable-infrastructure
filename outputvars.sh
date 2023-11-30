61e72f331549424683afce6c6824814ddb3de5c5
UltrViolet <endgameviable@gmail.com>
2023-11-30 14:15:33 -0500

    update metadata partition key

--- outputvars.sh
+++ outputvars.sh
@@ -1,5 +1,6 @@
 eval "$(<.env)"
-echo EGV_RESOURCE_EVENT_QUEUE=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep EventQueue | awk '{print $NF}')
-echo EGV_RESOURCE_JSON_BUCKET=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep JSONBucket | awk '{print $NF}')
-echo EGV_RESOURCE_SEARCH_TABLE=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep SearchTable | awk '{print $NF}')
-echo EGV_RESOURCE_STATE_TABLE=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep StateTable | awk '{print $NF}')
+echo EGV_RESOURCE_EVENT_QUEUE=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep EventQueue | awk '{print $NF}') > .env.local
+echo EGV_RESOURCE_JSON_BUCKET=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep JSONBucket | awk '{print $NF}') >> .env.local
+echo EGV_RESOURCE_SEARCH_TABLE=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep SearchTable | awk '{print $NF}') >> .env.local
+echo EGV_RESOURCE_STATE_TABLE=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep StateTable | awk '{print $NF}') >> .env.local
+cat .env.local
