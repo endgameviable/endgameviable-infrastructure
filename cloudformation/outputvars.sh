bcc0aa1c2286a230a6ef9ae6b28f20223aa76709
UltrViolet <endgameviable@gmail.com>
2023-12-03 14:42:17 -0500

    working infrastructure

--- cloudformation/outputvars.sh
+++ cloudformation/outputvars.sh
@@ -1,6 +1,6 @@
-eval "$(<.env)"
-echo EGV_RESOURCE_EVENT_QUEUE=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep EventQueue | awk '{print $NF}') > .env.local
-echo EGV_RESOURCE_JSON_BUCKET=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep JSONBucket | awk '{print $NF}') >> .env.local
-echo EGV_RESOURCE_SEARCH_TABLE=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep SearchTable | awk '{print $NF}') >> .env.local
-echo EGV_RESOURCE_STATE_TABLE=$(aws cloudformation describe-stacks --stack-name $STACKNAME --output text | grep StateTable | awk '{print $NF}') >> .env.local
+STACKNAME=endgameviable231203
+echo EGV_RESOURCE_EVENT_QUEUE=$(aws cloudformation describe-stacks --stack-name $STACKNAME-resource --output text | grep EventQueue | awk '{print $NF}') > .env.local
+echo EGV_RESOURCE_STATE_TABLE=$(aws cloudformation describe-stacks --stack-name $STACKNAME-resource --output text | grep StateTable | awk '{print $NF}') >> .env.local
+echo EGV_RESOURCE_JSON_BUCKET=$(aws cloudformation describe-stacks --stack-name $STACKNAME-build --output text | grep JSONBucket | awk '{print $NF}') >> .env.local
+echo EGV_RESOURCE_SEARCH_TABLE=$(aws cloudformation describe-stacks --stack-name $STACKNAME-build --output text | grep SearchTable | awk '{print $NF}') >> .env.local
 cat .env.local
--- emptybuckets.sh
+++
@@ -1,9 +0,0 @@
-# CloudFormation won't delete non-empty buckets.
-# Be careful with this, it just deletes them no questions asked.
-# But since we have these scripts to recreate everything from scratch,
-# it wouldn't be a big loss anyway.
-eval "$(<.env)"
-aws s3api list-buckets --output text | awk '{print $NF}' | grep ^$STACKNAME | while read -r bucket_name; do
-  echo "About to empty the bucket $bucket_name"
-  aws s3 rm s3://$bucket_name --recursive --quiet
-done
